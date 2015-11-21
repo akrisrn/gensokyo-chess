1392fafb73b836b5c1659cfce0a8e36e24a9d41e
AkrISrn <shairkarine@gmail.com>
2015-11-21 21:34:46 +0800

    附加 idea Gui 设计库和 Gui 程序打包相关

--- buildGui.sh
+++ buildGui.sh
@@ -5,6 +5,6 @@ if [ ! -d "tmp" ]; then
 fi
 
 javac -cp lib/javacsv.jar:lib/uiDesigner.jar -d tmp/ src/com/gensokyochess/*.java
-jar cfm gnc-2d.jar META-INF/MANIFEST.MF -C tmp/ .
+jar cfm gnc-2d-gui.jar META-INF/MANIFEST_GUI.MF -C tmp/ .
 
 rm -rf tmp
