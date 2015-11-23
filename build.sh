acb53976821ab89060b3e781832b0ba709b59e4d
AkrISrn <shairkarine@gmail.com>
2015-11-23 17:37:11 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
 version=$(git rev-list --all | head -n 1 | cut -b 1-5)
-if [ ! -e "gnc-2d-v${version}.jar" ]; then
-    rm -rf gnc-2d-v*.jar
+if [ ! -e "gnc-2d-v${version}.jar" ] || [ ! -e "gnc-2d-gui-v${version}.jar" ]; then
     if [ ! -d "tmp" ]; then
         mkdir tmp
     fi
     javac -encoding utf-8 -cp lib/javacsv.jar:lib/uiDesigner.jar -d tmp/ src/com/gensokyochess/*.java
-    jar cfm gnc-2d-v${version}.jar META-INF/MANIFEST.MF -C tmp/ .
+    if [ ! -e "gnc-2d-v${version}.jar" ]; then
+        rm -rf gnc-2d-v*.jar
+        jar cfm gnc-2d-v${version}.jar META-INF/MANIFEST.MF -C tmp/ .
+    fi
+    if [ ! -e "gnc-2d-gui-v${version}.jar" ]; then
+        rm -rf gnc-2d-gui-v*.jar
+        jar cfm gnc-2d-gui-v${version}.jar META-INF/MANIFEST_GUI.MF -C tmp/ .
+    fi
     rm -rf tmp
 fi
